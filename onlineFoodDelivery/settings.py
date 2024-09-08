95fa6b160936588a417749482b6e42eca93662f3
pandit-anil <info.anilpandit21@gmail.com>
2024-09-08 17:00:33 +0545

    Update settings.py
    for images

--- onlineFoodDelivery/settings.py
+++ onlineFoodDelivery/settings.py
@@ -188,8 +188,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR / 'media' 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 AUTH_USER_MODEL = 'useraccount.User'
 
